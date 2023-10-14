5675ea965423b5c026dc05948aa0ce882d81f7af
Mohsin Ali <hellomohsinhello@gmail.com>
2023-10-14 19:34:39 +0400

    add first test

---
+++ tests/TempDirTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hellomohsinhello\TempDir\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TempDirTest extends TestCase
+{
+
+    public function testTempDir()
+    {
+        $this->assertTrue(true);
+    }
+}
